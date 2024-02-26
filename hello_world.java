c560e9c0f87044f95e12e32015cca1051b3baeda
Belsproute <kozar.a.a.rk6@yandex.ru>
2024-02-26 23:16:46 +0300

    bugfix created

--- hello_world.java
+++ hello_world.java
@@ -1,6 +1,6 @@
 public class hello_world {
     public static void main(String[] args){
         System.out.println("Hello world!");
-        System.out.println("Changed smtng for git hw");
+        System.out.println("Changed something for git hw");
     }
 }
